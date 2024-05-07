efb5e35a2987e7420414ab34805b4310c11820cb
Ferdous Mannan Shajib <153799536+supahaxxa@users.noreply.github.com>
2024-05-08 00:49:18 +0600

    created a template for C++

---
+++ starter.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// define you functions from here
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  // enter your code from here
+
+  return 0;
+}
